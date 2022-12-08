ab7f8344f5df3d19bdc434e07d7728f3a2c6003d
Rachael McBride <mcbride.r@gmail.com>
2022-12-08 08:14:11 +0000

    Changes to requirements.txt and database table set up

--- sql/setup_database.sql
+++ sql/setup_database.sql
@@ -1,32 +1,32 @@
--- Table: learn.math
+-- Table: public.card
 
-DROP TABLE learn.card;
-
-CREATE TABLE card
-(
-	id serial PRIMARY KEY,
-	topic text COLLATE pg_catalog."default",
-    question text COLLATE pg_catalog."default",
-    answer text COLLATE pg_catalog."default",
-    difficulty integer,
-    last_date date,
-    score integer
-
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE card
-    OWNER to postgres;
-
-COMMENT ON TABLE card
-    IS 'Holds questions and answers for various topics';
-
-
-INSERT INTO card (topic, question, answer, difficulty, last_date, score)
-VALUES ('Maths', 'This is a test Maths question', 'This is a test Maths answer', 5, '2021-04-20', 100),
-('Maths', 'This is a second test Maths question', 'This is a second test Maths answer', 4, '2021-04-20', 10),
-('Programming', 'This is a test Programming question', 'This is a test Programming answer', 5, '2021-04-20', 100),
-('Programming', 'This is a second test Programming question', 'This is a second test Programming answer', 4, '2021-04-20', 10);
-
-select * from card;
+--DROP TABLE public.card;
+--
+--CREATE TABLE public.card (
+--	id serial4 NOT NULL,
+--	topic text NULL,
+--	question text NULL,
+--	answer text NULL,
+--	difficulty int4 NULL,
+--	last_date date NULL,
+--	score int4 NULL,
+--	CONSTRAINT card_pkey PRIMARY KEY (id),
+--	CONSTRAINT card_un UNIQUE (id)
+--);
+--
+--TABLESPACE pg_default;
+--
+--ALTER TABLE card
+--    OWNER to postgres;
+--
+--COMMENT ON TABLE card
+--    IS 'Holds questions and answers for various topics';
+--
+--
+--INSERT INTO card (topic, question, answer, difficulty, last_date, score)
+--VALUES ('Maths', 'This is a test Maths question', 'This is a test Maths answer', 5, '2021-04-20', 100),
+--('Maths', 'This is a second test Maths question', 'This is a second test Maths answer', 4, '2021-04-20', 10),
+--('Programming', 'This is a test Programming question', 'This is a test Programming answer', 5, '2021-04-20', 100),
+--('Programming', 'This is a second test Programming question', 'This is a second test Programming answer', 4, '2021-04-20', 10);
+--
+--select * from card;
